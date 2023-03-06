a2ff934dff206621bfb7f9a0ae35b9e73efaff5d
adamswonder <adamswill.i.am71002@gmail.com>
2023-03-06 11:13:30 +0300

    blog files generated and setup

---
+++ blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2023'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2022'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
