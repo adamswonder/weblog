e8bbd26d25168ea9d911effe06faf526d16aab8f
adamswonder <adamswill.i.am71002@gmail.com>
2023-03-14 20:29:45 +0300

    🔧fix:user model import

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.models import models
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
