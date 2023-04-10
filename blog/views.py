f5bf6dc2145112a997e1103ad52595cff246cd53
adamswonder <adamswill.i.am71002@gmail.com>
2023-04-10 23:02:49 +0300

    ✨Adds Post create view and routing

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from .models import Post
 
 
@@ -17,6 +17,10 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
