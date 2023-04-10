a26b17a92a0865becbb69b2321971a56a0946d60
adamswonder <adamswill.i.am71002@gmail.com>
2023-04-10 12:15:25 +0300

    🔧Fix:fixes profile email link

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) #creates a 1 to 1 relationship
     image = models.ImageField(default='default.jpg', upload_to='profile_pictures')
-    bio = models.TextField(default='Your autobiography goes here!')
+    bio = models.TextField(default='autobiography goes here!')
 
     def __str__(self):
         return f'{self.user.username} Profile'
