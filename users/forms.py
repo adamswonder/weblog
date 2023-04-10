d73f96fc7c8807049dd6e8f512a66800622e570f
adamswonder <adamswill.i.am71002@gmail.com>
2023-04-10 13:08:04 +0300

    💾Save: updates user specific details fields

--- users/forms.py
+++ users/forms.py
@@ -8,3 +8,11 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+#user model form to work with user profile during update
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
