83adf243ae1f3da3b386c1f10eb6792cce5c4a16
adamswonder <adamswill.i.am71002@gmail.com>
2023-04-10 11:18:57 +0300

    ✨Feat:add Profile model & initialize relationship

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) #creates a 1 to 1 relationship
+    image = models.ImageField(default='default.jpg', upload_to='profile_pictures')
+    bio = models.TextField(default='Your autobiography goes here!')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
