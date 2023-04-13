1460cd6f9e409cb1de136dc0f33d4cc464bfeaa4
adamswonder <adamswill.i.am71002@gmail.com>
2023-04-13 22:48:11 +0300

    🔧Fix:get_object and 404 shortcuts fix

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
@@ -25,7 +25,7 @@ class UserPostListView(ListView):
     paginate_by = 5
 
     def get_queryset(self):
-        user = get_list_or_404(User, username=self.kwargs.get('username')) #get username from url
+        user = get_object_or_404(User, username=self.kwargs.get('username')) #get username from url
         return Post.objects.filter(author=user).order_by('-date_posted') # retunrs specific user posts in desc order
 
 class PostDetailView(DetailView):
