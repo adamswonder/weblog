b6fae8409dd192c23eb0533e58ad802d31c06cd5
adamswonder <adamswill.i.am71002@gmail.com>
2023-04-13 22:06:41 +0300

    ✨Feat:adds query_set and filter for user specific blog

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
@@ -15,7 +16,17 @@ class PostListView(ListView):
     template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 2
+    paginate_by = 5
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_query_set(self):
+        user = get_list_or_404(User, username=self.kwargs.get('username')) #get username from url
+        return Post.objects.filter(author=user).order_by('-date_posted') # retunrs specific user posts in desc order
 
 class PostDetailView(DetailView):
     model = Post
